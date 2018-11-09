64fd2b44538d2f7f71af1827d233c14c3e65dcc2
ieee0824 <ieee0824@ezweb.ne.jp>
2018-11-09 11:11:40 +0900

    wip

--- tnp.go
+++ tnp.go
@@ -6,7 +6,7 @@ import (
 )
 
 var telPatternRegs = map[string][]*regexp.Regexp{
-	"fixedLinePhone": []*regexp.Regexp{
+	"fixed ;ine phone": []*regexp.Regexp{
 		regexp.MustCompile(`0[0-9]-[2-9]\d{3}-\d{4}`),
 		regexp.MustCompile(`0\d{2}-[2-9]\d{2}-\d{4}`),
 		regexp.MustCompile(`0\d{3}-[2-9]\d{1}-\d{4}`),
@@ -15,15 +15,22 @@ var telPatternRegs = map[string][]*regexp.Regexp{
 	"m2m": []*regexp.Regexp{
 		regexp.MustCompile(`020-([1-3]|[5-9])\d{2}-\d{5}`),
 	},
-	"pocket-bell": []*regexp.Regexp{
+	"pocket bell": []*regexp.Regexp{
 		regexp.MustCompile(`020-4\d{2}-\d{5}`),
 	},
-	"ip-phone": []*regexp.Regexp{
+	"ip phone": []*regexp.Regexp{
 		regexp.MustCompile(`050-[1-9]\d{3}-\d{4}`),
 	},
-	"mobile-phone": []*regexp.Regexp{
+	"mobile phone": []*regexp.Regexp{
 		regexp.MustCompile(`0[7-9]0-[1-9]\d{2}-\d{5}`),
 		regexp.MustCompile(`0[7-9]0-[1-9]\d{3}-\d{4}`),
+	},
+	"incoming charge": []*regexp.Regexp{
+		regexp.MustCompile(`0120-\d{3}-\d{3}`),
+		regexp.MustCompile(`0800-\d{3}-\d{3}`),
+	},
+	"unified number": []*regexp.Regexp{
+		regexp.MustCompile(`0570-\d{3}-\d{3}`),
 	},
 }
 
