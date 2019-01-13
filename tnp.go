966330c6ee74faf350264e42f874594aa6365193
ieee0824 <ieee0824@ezweb.ne.jp>
2019-01-13 13:30:50 +0900

    define type

--- tnp.go
+++ tnp.go
@@ -6,58 +6,87 @@ import (
 	"strings"
 )
 
-var ignoreTypes = []string{}
+type TelType int
 
-func SetIgnoreTypes(s ...string) {
-	ignoreTypes = append(ignoreTypes, s...)
+func (t TelType) String() string {
+	if t == -1 || len(typeNames) <= int(t) {
+		return "not tel number"
+	}
+	return typeNames[t]
 }
 
-func isIgnore(s string) bool {
+const (
+	FixedLinePhone TelType = iota
+	M2M
+	PocketBell
+	IPPhone
+	MobilePhone
+	IncomingCharge
+	UnifiedNumber
+)
+
+var typeNames = []string{
+	"fixed line phone",
+	"m2m",
+	"pocket bell",
+	"ip phone",
+	"mobile phone",
+	"incoming charge",
+	"unified number",
+}
+
+var ignoreTypes = []TelType{}
+
+func SetIgnoreTypes(t ...TelType) {
+	ignoreTypes = append(ignoreTypes, t...)
+}
+
+func isIgnore(t TelType) bool {
 	for _, v := range ignoreTypes {
-		if v == s {
+		if v == t {
 			return true
 		}
 	}
 	return false
 }
 
-var telPatternRegs = map[string][]*regexp.Regexp{
-	"fixed line phone": []*regexp.Regexp{
+var telPatternRegs = [][]*regexp.Regexp{
+	[]*regexp.Regexp{
 		regexp.MustCompile(`0[0-9]-[2-9]\d{3}-\d{4}`),
 		regexp.MustCompile(`0\d{2}-[2-9]\d{2}-\d{4}`),
 		regexp.MustCompile(`0\d{3}-[2-9]\d{1}-\d{4}`),
 		regexp.MustCompile(`0\d{4}-[2-9]-\d{4}`),
 	},
-	"m2m": []*regexp.Regexp{
+	[]*regexp.Regexp{
 		regexp.MustCompile(`020-([1-3]|[5-9])\d{2}-\d{5}`),
 	},
-	"pocket bell": []*regexp.Regexp{
+	[]*regexp.Regexp{
 		regexp.MustCompile(`020-4\d{2}-\d{5}`),
 	},
-	"ip phone": []*regexp.Regexp{
+	[]*regexp.Regexp{
 		regexp.MustCompile(`050-[1-9]\d{3}-\d{4}`),
 	},
-	"mobile phone": []*regexp.Regexp{
+	[]*regexp.Regexp{
 		regexp.MustCompile(`0[7-9]0-[1-9]\d{2}-\d{5}`),
 		regexp.MustCompile(`0[7-9]0-[1-9]\d{3}-\d{4}`),
 	},
-	"incoming charge": []*regexp.Regexp{
+	[]*regexp.Regexp{
 		regexp.MustCompile(`0120-\d{3}-\d{3}`),
 		regexp.MustCompile(`0800-\d{3}-\d{3}`),
 	},
-	"unified number": []*regexp.Regexp{
+	[]*regexp.Regexp{
 		regexp.MustCompile(`0570-\d{3}-\d{3}`),
 	},
 }
 
-func IsTelNumber(s string) (bool, string) {
+func IsTelNumber(s string) (bool, TelType) {
 	for k, rs := range telPatternRegs {
-		if isIgnore(k) {
+		if isIgnore(TelType(k)) {
 			continue
 		}
 		for _, r := range rs {
 			if r.Copy().MatchString(s) {
-				return true, k
+				return true, TelType(k)
 			}
 		}
 	}
@@ -66,12 +95,12 @@ func IsTelNumber(s string) (bool, string) {
 		return IsTelNumber(replaceParenthesis(s))
 	}
 
-	return false, ""
+	return false, -1
 }
 
 func CropTelNumber(s string) (string, error) {
 	for k, rs := range telPatternRegs {
-		if isIgnore(k) {
+		if isIgnore(TelType(k)) {
 			continue
 		}
 		for _, r := range rs {
